bdf390684377aee04854f26e7004f5686eb7b5f1
AlvaraTres <diego.leonardo.alvarado.salas@gmail.com>
2021-07-16 00:55:36 -0400

    edit reserva ok

--- app/Http/Livewire/ShowReservas.php
+++ app/Http/Livewire/ShowReservas.php
@@ -11,10 +11,12 @@ use Carbon\Carbon;
 class ShowReservas extends Component
 {
     public $open = false;
+    public $openEditModal = false;
 
     public $events = '';
 
     public $fecha_res;
+    public $reserva_id;
     //datos reserva
     public $id_usuario, $id_paciente, $fecha_reserva, $hora_reserva, $fecha_hora_reserva, $motivo_reserva, $cert_alumno_regular, $fecha_hora_reserva_fin;
 
@@ -81,5 +83,64 @@ class ShowReservas extends Component
             'fecha_hora_reserva',
             'open'
         ]);
+        return redirect()->to('/reservas');
+    }
+
+    public function editReserva($reserva){
+        $reserva_find = Reserva::where('fecha_hora_reserva', '=', $reserva['start'])->first();
+        //dd($reserva_find);
+        $this->reserva_id = $reserva_find->id;
+        $this->motivo_reserva = $reserva_find->motivo_reserva;
+        $this->fecha_reserva = Carbon::parse($reserva['start'])->format('d-m-Y');
+        $this->hora_reserva = Carbon::parse($reserva['start'])->format('H:i');
+        //dd($format_time);
+
+    }
+
+    public function cancelarEdit(){
+        $this->openEditModal = false;
+        return redirect()->to('/reservas');
+    }
+
+    public function updateReserva($editFecha, $editHora){
+        //dd($editFecha, $editHora);
+        $up_fecha = Carbon::parse($editFecha)->format('Y-m-d');
+        //$up_fecha_res_form = Carbon::createFromFormat('Y-m-d H:i:s', $up_fecha)->format('Y-m-d');
+        $up_time = Carbon::parse($editHora)->addSeconds('00')->format('H:i:s');
+        
+        $up_fecha_final = Carbon::createFromFormat('Y-m-d H:i:s',$up_fecha . ' ' . $up_time);
+        //dd($up_fecha_final);
+
+        $up_hora_termino = Carbon::parse($up_fecha_final)->addHour();
+
+        $reserva_up = Reserva::find($this->reserva_id);
+        //dd($reserva_up);
+        $reserva_up->update([
+            'fecha_reserva' => $up_fecha,
+            'hora_reserva' => $up_fecha_final,
+            'fecha_hora_reserva' => $up_fecha_final,
+            'fecha_hora_reserva_fin' => $up_hora_termino,
+        ]);
+
+        $this->reset([
+            'reserva_id',
+            'motivo_reserva',
+            'fecha_reserva',
+            'hora_reserva',
+            'openEditModal'
+        ]);
+
+        return redirect()->to('/reservas');
+    }
+
+    public function getReservas(){
+        $reservas = DB::table('reservas')
+                    ->join('users', 'users.id', '=', 'reservas.id_usuario')
+                    ->join('pacientes', 'pacientes.id', '=', 'reservas.id_paciente')
+                    ->where('reservas.id_paciente', '=', Auth::user()->id)
+                    ->select(DB::raw('CONCAT(pacientes.nombre_paciente, \' \', pacientes.ap_pat_paciente) AS title'), 'reservas.fecha_hora_reserva as start','reservas.motivo_reserva as description', 'reservas.fecha_hora_reserva_fin as end')
+                    ->get();
+
+        return json_encode($reservas);
     }
 }
--- resources/views/livewire/show-reservas.blade.php
+++ resources/views/livewire/show-reservas.blade.php
@@ -31,6 +31,33 @@
         </x-slot>
     </x-jet-dialog-modal>
 
+    <!-- MODAL DE Edit RESERVAS -->
+    <x-jet-dialog-modal wire:model="openEditModal" id="edit_res">
+        <x-slot name="title">
+            Cambiar Reserva de Hora
+        </x-slot>
+
+        <x-slot name="content">
+            <x-jet-label value="Fecha de Reserva: " />
+            <input type="text" wire:model.defer="fecha_reserva" class="datetimepicker-input border rounded-md border-gray-300" id="datetimepickerFecha" data-toggle="datetimepicker"
+                data-target="#datetimepickerFecha" />
+
+            <x-jet-label value="Hora Reserva:" />            
+            <input type="text" wire:model.defer="hora_reserva" class="datetimepicker-input border rounded-md border-gray-300" id="datetimepickerEdit" data-toggle="datetimepicker"
+                data-target="#datetimepickerEdit" />
+            <x-jet-input-error for="hora_reserva" />
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:loading.attr="disabled" class="disabled:opacity-25" id="btn_delete_reserva">Cancelar Reserva
+            </x-jet-secondary-button>
+            <x-jet-secondary-button wire:loading.attr="disabled" class="disabled:opacity-25 bg-blue-500 text-white" id="btn_edit_reserva">Modificar Reserva
+            </x-jet-secondary-button>
+            <x-jet-danger-button wire:click="cancelarEdit" wire:loading.attr="disabled"
+                class="disabled:opacity-25">Cancelar</x-jet-secondary-button>
+        </x-slot>
+    </x-jet-dialog-modal>
+
     @push('js')
         <script>
             document.addEventListener('livewire:load', function() {
@@ -52,7 +79,7 @@
 
                     dateClick: function(info) {
                         //alert('Click: '+info.timeStr);
-                        @this.set('open', true);;
+                        @this.set('open', true);
                         var date = new Date(info.dateStr + 'T00:00:00');
                         //alert(description);
                         document.getElementById("btn_reserva").addEventListener("click", function() {
@@ -71,10 +98,37 @@
                                 description: description
                             };
                             @this.storeReserva(eventAdd, startTime);
+                            calendar.refetchEvents();
                         });
+                    },
+                    eventClick: function(info){
+                        var reserva = info.event;
+                        console.log(reserva);
+                        @this.editReserva(reserva);
+                        @this.set('openEditModal', true);
+                        document.getElementById('btn_edit_reserva').addEventListener("click", function(){
+                           var editFecha = document.getElementById('datetimepickerFecha').value;
+                           var editHora = document.getElementById('datetimepickerEdit').value;
+                           console.log(editHora);
+                           @this.updateReserva(editFecha, editHora);
+                           calendar.refetchEvents();
+                        });
+                    },
+                    loading: function(isLoading){
+                        if(!isLoading){
+                            //resetear eventos
+                            this.getEvents().forEach(function(e){
+                                if(e.source === null){
+                                    e.remove();
+                                }
+                            });
+                        }
                     }
                 });
                 calendar.render();
+                @this.on('refreshCalendar', () => {
+                    calendar.refetchEvents()
+                });
             });
         </script>
         <script type="text/javascript">
@@ -100,6 +154,56 @@
                 minTime: '10:00:00',
                 maxTime: '20:00:00',
                 format: 'H:i'
+            });
+        </script>
+        <!-- Time picker edición -->
+        <script type="text/javascript">
+            jQuery.datetimepicker.setLocale('es');
+            jQuery('#datetimepickerEdit').datetimepicker({
+                i18n: {
+                    de: {
+                        months: [
+                            'Enero', 'Febrero', 'Marzo', 'Abril',
+                            'Mayo', 'Junio', 'Julio', 'Agosto',
+                            'Septiembre', 'Octubre', 'Noviembre', 'Diciembre',
+                        ],
+                        dayOfWeek: [
+                            "So.", "Mo", "Di", "Mi",
+                            "Do", "Fr", "Sa.",
+                        ]
+                    }
+                },
+                container: '#datetimepickerEdit',
+                orientation: "auto-top",
+                datepicker: false,
+                timepicker: true,
+                minTime: '10:00:00',
+                maxTime: '20:00:00',
+                format: 'H:i'
+            });
+        </script>
+        <!-- DateTimePicker cambio de fecha MODAL EDIT -->
+        <script type="text/javascript">
+            jQuery.datetimepicker.setLocale('es');
+            jQuery('#datetimepickerFecha').datetimepicker({
+                i18n: {
+                    de: {
+                        months: [
+                            'Enero', 'Febrero', 'Marzo', 'Abril',
+                            'Mayo', 'Junio', 'Julio', 'Agosto',
+                            'Septiembre', 'Octubre', 'Noviembre', 'Diciembre',
+                        ],
+                        dayOfWeek: [
+                            "So.", "Mo", "Di", "Mi",
+                            "Do", "Fr", "Sa.",
+                        ]
+                    }
+                },
+                container: '#datetimepickerFecha',
+                orientation: "auto-top",
+                datepicker: true,
+                timepicker: false,
+                format: 'd-m-Y'
             });
         </script>
     @endpush
