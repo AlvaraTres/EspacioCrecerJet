2a6daad95a3bb1c55a44f1907f3d4d562438f9e5
AlvaraTres <diego.leonardo.alvarado.salas@gmail.com>
2021-07-28 23:13:59 -0400

    cambios en front end guest auth

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,4 +1,4 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pb-20 sm:pt-0 bg-gray-100">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pb-20 sm:pt-0" style="background-image: url(images/logo_opacidad_40.png); overflow: hidden; background-size: auto; background-position: center;">
     <div>
         {{ $logo }}
     </div>
@@ -6,24 +6,22 @@
     @if (Request::is('login'))
         <div class="bg-blue-500 sm:max-w-md shadow-md w-full overflow-hidden sm:rounded-lg">
             <p class="font-bold text-white text-center mt-2 text-lg h-2">Iniciar Sesión</p>
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg"
-                style="background-image: url(images/LogoEspacioCrecer.jpeg); overflow: hidden; background-size: cover; background-position: center;">
+            <div class="w-full bg-gray-50 sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-b-lg"
+                >
                 {{ $slot }}
             </div>
         </div>
     @elseif(Request::is('forgot-password'))
         <div class="bg-blue-500 sm:max-w-md shadow-md w-full overflow-hidden sm:rounded-lg">
             <p class="font-bold text-white text-center mt-2 text-lg h-2">Recuperar Contraseña</p>
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg"
-                style="background-image: url(images/LogoEspacioCrecer.jpeg); overflow: hidden; background-size: cover; background-position: center;">
+            <div class="w-full bg-gray-50 sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-b-lg">
                 {{ $slot }}
             </div>
         </div>
     @elseif(Request::is('register'))
         <div class="mt-20 bg-blue-500 sm:max-w-md shadow-md w-full overflow-hidden sm:rounded-lg">
             <p class="font-bold text-white text-center mt-2 text-lg h-2">Registrarse</p>
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg"
-                style="background-image: url(images/LogoEspacioCrecer.jpeg); overflow: hidden; background-size: cover; background-position: center;">
+            <div class="w-full bg-gray-50 sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-b-lg">
                 {{ $slot }}
             </div>
         </div>
