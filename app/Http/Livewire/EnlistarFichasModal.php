c9b83836b4d8de041c4d89bf907ea0d45ba34492
AlvaraTres <diego.leonardo.alvarado.salas@gmail.com>
2021-08-07 04:50:34 -0400

    subir ficha en archivo pdf doc docx a base de datos

--- app/Http/Livewire/EnlistarFichasModal.php
+++ app/Http/Livewire/EnlistarFichasModal.php
@@ -4,16 +4,24 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Paciente;
+use App\Models\Fichapaciente;
 use Carbon\Carbon;
 use DB;
 use PDF;
+use Livewire\WithFileUploads;
 
 class EnlistarFichasModal extends Component
 {
+    use WithFileUploads;
+
     public $openVerFichasModal = false;
+    public $openUploadFileModal = false;
+    public $showInfoFichasModal = true;
     public $paciente;
     public $paciente_rut;
     public $count_fichas=1;
+
+    public $id_usuario, $id_paciente, $resumen_atencion, $archivo, $fecha_atencion_ficha;
 
     public function mount(Paciente $paciente){
         $this->paciente = $paciente;
@@ -64,5 +72,49 @@ class EnlistarFichasModal extends Component
 
         $ficha_pdf = PDF::setOptions(['isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true])->loadView('livewire.fichaPdf', compact('datos_ficha', 'pic', 'edad_calculada'));
         return $ficha_pdf->stream('ficha.pdf');
+    }
+
+    public function showUploadFileModal()
+    {
+        $this->showInfoFichasModal = false;
+        $this->openUploadFileModal = true;
+    }
+
+    public function closeUploadModal()
+    {
+        $this->openVerFichasModal = true;
+        $this->openUploadFileModal = false;
+    }
+
+    public function saveFileFichaPaciente()
+    {
+        $this->validate([
+            'archivo' => 'max:1024',
+            'fecha_atencion_ficha' => 'required'
+        ]);
+
+        $fecha = Carbon::parse($this->fecha_atencion_ficha)->format('Y-m-d');
+
+        //dd($this->archivo);
+
+        $ficha = Fichapaciente::create([
+                    'id_usuario' => \Auth::user()->id,
+                    'id_paciente' => $this->paciente->id,
+                    'fecha_atencion_ficha' => $fecha,
+                    'resumen_atencion' => 'sin especificar'
+                ]);
+        
+        if($this->archivo != null){
+            $file = $this->archivo->getClientOriginalName();
+            $this->archivo->storeAs('subfolder/' . $this->paciente->nombre_paciente, $file);
+            $ficha->update(['archivo' => $file]);
+        }
+
+        $this->reset([
+            'archivo',
+            'fecha_atencion_ficha',
+            'openVerFichasModal',
+            'openUploadFileModal'
+        ]);
     }
 }
--- resources/views/livewire/enlistar-fichas-modal.blade.php
+++ resources/views/livewire/enlistar-fichas-modal.blade.php
@@ -1,6 +1,8 @@
 <div>
-    <a href="#" class="px-8 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-500 focus:outline-none focus:border-green-700 focus:ring focus:ring-green-200 active:bg-green-600 disabled:opacity-25 transition'" wire:click="$set('openVerFichasModal', true)" wire:loading.attr="disabled"
-    class="disabled:opacity-25">Fichas</a>
+    <a href="#"
+        class="px-8 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-500 focus:outline-none focus:border-green-700 focus:ring focus:ring-green-200 active:bg-green-600 disabled:opacity-25 transition'"
+        wire:click="$set('openVerFichasModal', true)" wire:loading.attr="disabled"
+        class="disabled:opacity-25">Fichas</a>
 
     <!-- MODAL FICHAS PACIENTES -->
     <x-jet-dialog-modal wire:model="openVerFichasModal">
@@ -10,8 +12,10 @@
             </div>
         </x-slot>
         <x-slot name="content">
-            <div class="container mt-4 mx-auto place-items-center">
-                <div class="grid grid-cols-1 md:grid-cols-1 gap-2 items-center">
+
+            @if ($showInfoFichasModal == true)
+                <div class="container mt-4 mx-auto place-items-center">
+                    <div class="grid grid-cols-1 md:grid-cols-1 gap-2 items-center">
                         @foreach ($ficha_paciente as $ficha)
                             <div class="flex items-center justify-center">
                                 <h1 class="font-semibold items-center mr-4">Fecha de atención:
@@ -22,19 +26,61 @@
                                     Ficha</a>
                             </div>
                         @endforeach
+                    </div>
                 </div>
-            </div>
-            @if(!$ficha_paciente->count())
-            <div class="flex items-stretch mb-4 mt-4">
-                <h1 class="font-semibold">No hay registros de fichas para este paciente.</h1>
-            </div>
+                @if (!$ficha_paciente->count())
+                    <div class="flex items-stretch mb-4 mt-4">
+                        <h1 class="font-semibold">No hay registros de fichas para este paciente.</h1>
+                    </div>
+                @endif
             @endif
+
+
+            <div class="text-left">
+                @if ($openUploadFileModal == true)
+                    <x-jet-label value="Fecha de atención:" />
+                @endif
+
+                <input @if ($openUploadFileModal == true) type="text" @else type="hidden" @endif
+                    class="datetimepicker-input border rounded-md border-gray-300"
+                    id="fechaAtencion{{ $paciente->id }}" data-toggle="datetimepicker"
+                    data-target="#fechaAtencion{{ $paciente->id }}" autocomplete="off" />
+                @if ($openUploadFileModal == true)
+                    <x-jet-label class="mt-4" value="Subir archivo:" />
+                    <input type="file" name="archivo" id="archivo" wire:model="archivo" accept=".pdf , .doc , .docx">
+                    @error('archivo')<span class="error">{{$message}}</span>@enderror
+                @endif
+            </div>
+
         </x-slot>
 
         <x-slot name="footer">
+            @if ($showInfoFichasModal == true)
+                <x-jet-danger-button wire:click="showUploadFileModal"
+                    class="px-8 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200 active:bg-blue-600 disabled:opacity-25 transition">
+                    Crear Ficha</x-jet-danger-button>
+            @endif
+            @if ($openUploadFileModal == true)
+                <x-jet-danger-button wire:click="saveFileFichaPaciente"
+                    class="px-8 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200 active:bg-blue-600 disabled:opacity-25 transition">
+                    Subir Ficha</x-jet-danger-button>
+            @endif
             <x-jet-danger-button wire:click="$set('openVerFichasModal', false)" wire:loading.attr="disabled"
                 class="disabled:opacity-25">Cerrar</x-jet-danger-button>
         </x-slot>
 
     </x-jet-dialog-modal>
+
+    @push('js')
+        <script type="text/javascript">
+            jQuery.datetimepicker.setLocale('es');
+            jQuery('#fechaAtencion{{ $paciente->id }}').datetimepicker({
+                timepicker: false,
+                format: 'd-m-Y',
+                maxDate: '0',
+            }).on('change', function(e) {
+                @this.set('fecha_atencion_ficha', jQuery('#fechaAtencion{{ $paciente->id }}').val());
+            });
+        </script>
+    @endpush
 </div>
